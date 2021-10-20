f587c03ac29891727015b29f1fc5db07c9b21fc4
ranaviira <84rent@gmail.com>
2021-10-20 14:21:16 +0400

    test ParsingString and MathematicalCalculations classes

--- src/MathematicalCalculations.java
+++ src/MathematicalCalculations.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MathematicalCalculations {
+
+    public static double calculation(String s) {
+        ArrayList<String> arrayList = ParsingString.parsing(s);
+        Stack<Double> stack = new Stack<>();
+
+        for (String string : arrayList) {
+            if (isNumber(string)) {
+                stack.push(Double.parseDouble(string));
+            } else {
+                double tmp1 = stack.pop();
+                double tmp2 = stack.pop();
+
+                switch (string) {
+                    case "+":
+                        stack.push(tmp1 + tmp2);
+                        break;
+                    case "-":
+                        stack.push(tmp2 - tmp1);
+                        break;
+                    case "*":
+                        stack.push(tmp1 * tmp2);
+                        break;
+                    case "/":
+                        try {
+                            stack.push(tmp2 / tmp1);
+                        } catch (ArithmeticException e) {
+                            System.out.println("Недопустимая операция, деление на ноль");
+                        }
+
+
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static boolean isNumber(String string) {
+        try {
+            Double.parseDouble(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
--- src/ParsingString.java
+++ src/ParsingString.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class ParsingString {
+
+    public static ArrayList<String> parsing(String string) {
+
+        String[] strings = string.split(" ");
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+
+        for (String s : strings) {
+            if (MathematicalCalculations.isNumber(s)) {
+                arrayList.add(s);
+            } else {
+                if (getPriority(s) >= 1) {
+                    while (stack.size() != 0) {
+                        if (getPriority(stack.peek()) >= getPriority(s)) {
+                            arrayList.add(stack.pop());
+                        } else break;
+                    }
+                }
+                stack.push(s);
+            }
+        }
+        while (!stack.empty()) {
+            arrayList.add(stack.pop());
+        }
+        return arrayList;
+    }
+
+
+    private static int getPriority(String string) {
+        if (string.equals("*") || (string.equals("/"))) {
+            return 2;
+        } else if (string.equals("+") || (string.equals("-"))) {
+            return 1;
+        } else return 0;
+    }
 }
